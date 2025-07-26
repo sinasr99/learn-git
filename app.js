b54be9e353f63a0e6dafe513b2ea22d985dc85fb
Sina Saber <sina.sr.21.09.1383@gmail.com>
2025-07-26 07:34:08 +0330

    add login #1

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 alert("hello world")
 
 const logout = () => alert("logout")
+
+const login = (username, password) => {
+    if (username && password) {
+        return true
+    }
+    return false
+}
